76d572a8c68db4e9e1d3270bb696b4a6a489947f
Elad Cohen <eladc.android@gmail.com>
2022-04-23 23:27:45 +0300

    Add GlobalComponents support

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,17 @@
 import type { App } from "vue";
+import VIdle from "../src/components/vidle";
 export interface InstallationOptions {
   name?: string;
 }
 declare namespace VIdle3 {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export function install(app: App, options: InstallationOptions): any;
+}
+
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+    VIdle: typeof VIdle;
+  }
 }
 
 export default VIdle3;
